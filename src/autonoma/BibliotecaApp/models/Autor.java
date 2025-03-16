b6b5fcf9f454b973995fe53bdba72692cda64ed1
Heily Rios <heilyayala005@gmail.com>
2025-03-16 01:06:22 -0500

    Agregar set y get a autor y el autor a las clases y ventanas

--- src/autonoma/BibliotecaApp/models/Autor.java
+++ src/autonoma/BibliotecaApp/models/Autor.java
@@ -1,6 +1,12 @@
 
 package autonoma.BibliotecaApp.models;
-
+/* @author  Heily Yohana Rios Ayala<heilyy.riosa@autonoma.edu.co>
+ * * 
+ * @version 1.0.0
+ * 
+ * @since 20250312
+ 
+ */
 
 public class Autor extends Persona {
     private String editorial;
@@ -9,6 +15,22 @@ public class Autor extends Persona {
     public Autor(String editorial, String profesion, String nombre, String documentoIdentidad, String correoElectronico) {
         super(nombre, documentoIdentidad, correoElectronico);
         this.editorial = editorial;
+        this.profesion = profesion;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
         this.profesion = profesion;
     }
     
