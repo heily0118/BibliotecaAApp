c982d5ad902e1f2920bf904750c915fe74299827
YogoPersonal <juansearias21@gmail.com>
2025-03-15 21:05:36 -0500

    Actualizacion de Persona.java

--- src/autonoma/BibliotecaApp/models/Persona.java
+++ src/autonoma/BibliotecaApp/models/Persona.java
@@ -6,8 +6,48 @@ package autonoma.BibliotecaApp.models;
 
 /**
  *
- * @author Santiago Uribe
+ * @author Sebastian Arias
  */
-public class Persona {
+public abstract class Persona {
     
+    private String nombre;
+    private String documentoIdentidad;
+    private String correoElectronico;
+    
+    public Persona(String nombre, String documentoIdentidad, String correoElectronico){
+        this. nombre = nombre;
+        this.documentoIdentidad = documentoIdentidad;
+        this.correoElectronico = correoElectronico;
+    }
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Documento: " + documentoIdentidad + ", Correo: " + correoElectronico;
+    }
 }
+
